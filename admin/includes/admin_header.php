f8cd34a4693cacef12030b72d162bbd169ad911c
ChristoT98 <christolakshan@gmail.com>
2022-03-27 16:02:02 +0530

    logout function created

--- admin/includes/admin_header.php
+++ admin/includes/admin_header.php
@@ -6,10 +6,16 @@
 
 <?php
 
-if(isset($_SESSION['user_role'])) {
-    if($_SESSION['user_role'] !== 'Admin') {
-        header('Location: ../index.php');
-    }
+if(!isset($_SESSION['user_role'])) {
+
+    header('Location: ../index.php');
+
+}else {
+
+if($_SESSION['user_role'] !== 'Admin') {
+    header('Location: ../index.php');
+}
+
 }
 
 ?>
--- includes/login.php
+++ includes/login.php
@@ -18,25 +18,24 @@ if (isset($_POST['login']))
     }
 
     while($row = mysqli_fetch_array($get_user_by_username)) {
-        echo $db_user_id = $row['user_id'];
-        echo $db_username = $row['username'];
-        echo $db_user_password = $row['user_password'];
-        echo $db_user_firstname = $row['user_firstname'];
-        echo $db_user_lastname = $row['user_lastname'];
-        echo $db_user_role= $row['user_role'];
+        $db_user_id = $row['user_id'];
+        $db_username = $row['username'];
+        $db_user_password = $row['user_password'];
+        $db_user_firstname = $row['user_firstname'];
+        $db_user_lastname = $row['user_lastname'];
+        $db_user_role= $row['user_role'];
     }
 
-    if($username !== $db_username && $password !== $db_user_password) {
-        header("Location: ../index.php");
-    } else if ($username == $db_username && $password == $db_user_password) {
+    if($username === $db_username && $password === $db_user_password) {
 
         $_SESSION['username'] = $db_username;
         $_SESSION['firstname'] = $db_user_firstname;
         $_SESSION['lastname'] = $db_user_lastname;
         $_SESSION['user_role'] = $db_user_role;
-        
+
         header("Location: ../admin");
-    }else {
+
+    } else {
         header("Location: ../index.php");
     }
 ?>
