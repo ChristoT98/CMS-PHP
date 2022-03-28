1b3ae995982dcffa678971ba224f152ae90c8503
ChristoT98 <christolakshan@gmail.com>
2022-03-28 09:05:09 +0530

    get user data to profile

---
+++ admin/profile.php
@@ -0,0 +1,130 @@
+<?php include "includes/admin_header.php"; ?>
+
+<?php
+
+if (isset($_SESSION['username'])) {
+    $username = $_SESSION['username'];
+
+    $query = "SELECT * FROM users WHERE username = '{$username}'";
+
+    $get_user_for_profile = mysqli_query($connection, $query);
+
+    while ($row = mysqli_fetch_array($get_user_for_profile)) {
+        $user_id = $row['user_id'];
+        $username = $row['username'];
+        $user_password = $row['user_password'];
+        $user_image = $row['user_image'];
+        $user_firstname = $row['user_firstname'];
+        $user_lastname = $row['user_lastname'];
+        $user_email = $row['user_email'];
+        $user_role = $row['user_role'];
+    }
+}
+
+?>
+
+<div id="wrapper">
+
+    <!-- Navigation -->
+    <?php include "includes/admin_navigation.php"; ?>
+
+    <div id="page-wrapper">
+
+        <div class="container-fluid">
+
+            <!-- Page Heading -->
+            <div class="row">
+                <div class="col-lg-12">
+                    <h1 class="page-header">
+                        Welcome Admin
+                        <small>Christo!</small>
+                    </h1>
+
+                    <form action="" method="post" enctype="multipart/form-data">
+                        <div class="form-group">
+                            <label for="user_firstname">First Name</label>
+                            <input type="text" value="<?php if(isset($user_firstname)){ echo $user_firstname;} ?>"
+                                class="form-control" placeholder="Enter First Name" name="user_firstname">
+                        </div>
+                        <div class="form-group">
+                            <label for="user_lastname">Last Name </label>
+                            <input type="text" value="<?php if(isset($user_lastname)){ echo $user_lastname;} ?>"
+                                class="form-control" placeholder="Enter Last Name" name="user_lastname">
+                        </div>
+                        <div class="form-group">
+                            <label for="user_email">Email </label>
+                            <input type="text" value="<?php if(isset($user_email)){ echo $user_email;} ?>"
+                                class="form-control" placeholder="Enter Email" name="user_email">
+                        </div>
+                        <div class="form-group">
+                            <label for="user_role">User Role</label>
+                            <select name="user_role" id="user_role" class="form-control">
+                                <option value="<?php echo $user_role; ?>">
+                                    <?php echo $user_role; ?>
+                                </option>
+                                <?php
+                if($user_role == 'Admin') {
+                    echo "<option value='Subscriber'>Subscriber</option>";
+                } else {
+                    echo "<option value='Admin'>Admin</option>";
+                }
+            ?>
+                            </select>
+                        </div>
+                        <!-- <div class="form-group">
+        <label for="user_image">User Image </label>
+        <img width="64" src="../images/<?php /*echo $user_image;*/ ?>" alt="">
+        <br><br>
+        <input  type="file" name="user_image">
+        
+    </div> -->
+                        <div class="form-group">
+                            <label for="username">Username </label>
+                            <input type="text" value="<?php if(isset($username)){ echo $username;} ?>"
+                                class="form-control" placeholder="Enter Username" name="username">
+                        </div>
+                        <div class="form-group">
+                            <label for="user_password">Password </label>
+                            <input type="password" value="<?php if(isset($user_password)){ echo $user_password;} ?>"
+                                class="form-control" name="user_password" placeholder="Enter New Password"
+                                name="user_password">
+                        </div>
+
+                        <div class="form-group">
+                            <input type="submit" class="btn btn-primary" name="update_profile" value="Update Profile">
+                        </div>
+                    </form>
+
+                    <?php 
+                        
+                        // if(isset($_GET['source'])) {
+                        //     $source = $_GET['source'];
+                        // } else {
+                        //     $source = '';
+                        // }
+
+                        // switch($source){
+                        //     case 'add_user';
+                        //     include "includes/add_user.php";
+                        //     break;
+
+                        //     case 'edit_user';
+                        //     include "includes/edit_user.php";
+                        //     break;
+
+                        //     default: include "includes/view_all_users.php";
+                        //     break;
+                        // }
+                        
+                        ?>
+                </div>
+            </div>
+            <!-- /.row -->
+
+        </div>
+        <!-- /.container-fluid -->
+
+    </div>
+    <!-- /#page-wrapper -->
+
+    <?php include "includes/admin_footer.php"; ?>
